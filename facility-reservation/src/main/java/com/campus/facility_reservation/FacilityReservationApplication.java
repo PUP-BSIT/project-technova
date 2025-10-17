dd4c454c02aa8a7239ebc97ffcfc221502a4c531
relente-patriciajoy <relente.patriciajoy@gmail.com>
2025-10-17 21:01:32 +0800

    refactor application entry point

--- facility-reservation/src/main/java/com/campus/facility_reservation/FacilityReservationApplication.java
+++ facility-reservation/src/main/java/com/campus/facility_reservation/FacilityReservationApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FacilityReservationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FacilityReservationApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FacilityReservationApplication.class, args);
+    }
 }
