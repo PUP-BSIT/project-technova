9a794c2e8b6ed4fd9a461587d589cbcd0738f691
relente-patriciajoy <relente.patriciajoy@gmail.com>
2025-10-17 20:50:37 +0800

    add maven project

---
+++ facility-reservation/src/main/java/com/campus/facility_reservation/FacilityReservationApplication.java
@@ -0,0 +1,13 @@
+package com.campus.facility_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilityReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilityReservationApplication.class, args);
+	}
+
+}
