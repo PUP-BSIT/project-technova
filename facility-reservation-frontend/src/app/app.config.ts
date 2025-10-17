12f2e9bd29b77610e238903e82f0131092385f56
relente-patriciajoy <relente.patriciajoy@gmail.com>
2025-10-17 22:00:19 +0800

    feat(project-frontend): add angular project
    * feat(project--frontend): add angular project
    
    * add files

---
+++ facility-reservation-frontend/src/app/app.config.ts
@@ -0,0 +1,24 @@
+// import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideBrowserGlobalErrorListeners(),
+//     provideZonelessChangeDetection(),
+//     provideRouter(routes)
+//   ]
+// };
+
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()
+  ]
+};
