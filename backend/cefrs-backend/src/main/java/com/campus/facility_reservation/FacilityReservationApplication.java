9719d26d01aca20644dac0f90021e0c95a672d30
relente-patriciajoy <relente.patriciajoy@gmail.com>
2025-11-01 22:00:16 +0800

    remove test annotations

--- backend/cefrs-backend/src/main/java/com/campus/facility_reservation/FacilityReservationApplication.java
+++ backend/cefrs-backend/src/main/java/com/campus/facility_reservation/FacilityReservationApplication.java
@@ -2,10 +2,8 @@ package com.campus.facility_reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
-@ActiveProfiles("test")
 public class FacilityReservationApplication {
 
     public static void main(String[] args) {
