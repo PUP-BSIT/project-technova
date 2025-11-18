169380a628444446d07dedeaa453a63c15bad5f2
relente-patriciajoy <relente.patriciajoy@gmail.com>
2025-11-18 19:17:33 +0800

    test(reset-password): add HttpClientTestingModule and RouterTestingModule to ResetPassword tests

--- frontend/cefrs-frontend/src/app/components/admin/reset-password/reset-password.spec.ts
+++ frontend/cefrs-frontend/src/app/components/admin/reset-password/reset-password.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { ResetPassword } from './reset-password';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ResetPassword', () => {
   let component: ResetPassword;
@@ -8,7 +10,11 @@ describe('ResetPassword', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ResetPassword],
+      imports: [
+        ResetPassword,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
       providers: [provideHttpClient()]
     })
       .compileComponents();
